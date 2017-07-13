7c77fe6d133e25783bf0cadd4e46b43b42bb89cd
Korawit <korawit.p@gmail.com>
2017-07-13 18:27:45 +0700

    Add get by location

--- catcher.js
+++ catcher.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-var instagram = require('./lib/instagram');
+var igtag = require('./lib/igtag');
+var igloc = require('./lib/igloc');
 var config = require('./config');
 
 // Connect to DB
@@ -9,14 +10,36 @@ mongoose.connect('mongodb://localhost:27017/instagram', {
 // Set custom promise to Bluebird
 mongoose.Promise = require('bluebird');
 
-function intervalFunc() {
-
-    for(var tag in config.hastags){        
-        var url = `https://www.instagram.com/explore/tags/${config.hastags[tag]}/`;
-        instagram.get(url, function (message) {
-            console.log(`\ntime: ${Date()}\nhashtag: ${config.hastags[tag]}\nstatus: ${message.status}\n`);
-        });        
+function startCatch() {
+    //Get by Tags
+    try {
+        for(var tag in config.hastags){        
+            var url = `https://www.instagram.com/explore/tags/${config.hastags[tag]}/`;
+            igtag.get(url, function (message) {
+                console.log(`\ntime: ${Date()}\nhashtag: ${config.hastags[tag]}\nstatus: ${message.status}\n`);
+            });        
+        }        
+    } catch (error) {
+        console.log(error);
     }
+
+    //Get by Locations
+    try {
+        for(var loc in config.locations){        
+            var url = `https://www.instagram.com/explore/locations/${config.locations[loc]}/`;
+            igloc.get(url, function (message) {
+                console.log(`\ntime: ${Date()}\nlocation: ${config.locations[loc]}\nstatus: ${message.status}\n`);
+            });        
+        }        
+    } catch (error) {
+        console.log(error);
+    }    
+}
+
+console.log('Start catching ...');
+startCatch();
+function intervalFunc() {
+    startCatch();
 }
 setInterval(intervalFunc, config.interval);
 
