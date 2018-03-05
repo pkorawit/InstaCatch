014c5f62674382a7fe3f952bd464854ad3e0693b
Korawit <korawit.p@gmail.com>
2018-03-05 16:13:23 +0700

    Added fetch by hasgtag

--- catcher.js
+++ catcher.js
@@ -16,42 +16,73 @@ mongoose.Promise = require('bluebird');
 
 async function fetch() {
 
-    for(var loc in config.locations){     
-        var locationdata = await Instagram.getDataByLocation(config.locations[loc]);  
+    //Fetch by Location
+    for (var loc in config.locations) {
+        var locationdata = await Instagram.getDataByLocation(config.locations[loc]);
         var entries = locationdata.location.media.nodes;
-        console.log('Found entries: ' + entries.length);
-        for(var i = 0; i < entries.length; i++){
-            var postdata = await Instagram.getPostByShortcode(entries[i].code);                        
-           
+        console.log('Location[' + config.locations[loc] + '] entries: ' + entries.length);
+        for (var i = 0; i < entries.length; i++) {
+            var postdata = await Instagram.getPostByShortcode(entries[i].code);
+
             //Store data to DB
             var postdb = new PostDocument;
             //Check if the selecte entry is existed in database
-            PostDocument.findOne({shortcode: postdata.shortcode}, function (err, exist){ 
-                if(exist){
+            PostDocument.findOne({
+                shortcode: postdata.shortcode
+            }, function (err, exist) {
+                if (exist) {
                     console.log(postdata.shortcode + " : already existed");
-                }
-                else{
+                } else {
                     //Insert new data to db 
                     postdb.created_at = Date.now();
                     postdb.shortcode = postdata.shortcode;
                     postdb.media = postdata;
                     postdb.save(function (err) {
-                        if (err) return handleError(err);                        
+                        if (err) return handleError(err);
                         console.log(postdata.shortcode + " : saved");
                     });
                 }
-            }); 
-        } 
-        console.log(`\ntime: ${Date()}\nlocation: ${config.locations[loc]}\nstatus: finished\n`);           
-    }        
+            });
+        }
+        console.log(`\ntime: ${Date()}\nlocation: ${config.locations[loc]}\nstatus: finished\n`);
+    }
+
+    //Fetch by Hashtag
+    for (var tag in config.hastags) {
+        var hashtagdata = await Instagram.getDataByHashtag(config.hastags[tag]);
+        var entries = hashtagdata.edges;
+        console.log('Hashtag[' + config.locations[loc] + '] entries: ' + entries.length);
+        for (var i = 0; i < entries.length; i++) {
+            var postdata = await Instagram.getPostByShortcode(entries[i].node.shortcode);
+            //Store data to DB
+            var postdb = new PostDocument;
+            //Check if the selecte entry is existed in database
+            PostDocument.findOne({
+                shortcode: postdata.shortcode
+            }, function (err, exist) {
+                if (exist) {
+                    console.log(postdata.shortcode + " : already existed");
+                } else {
+                    //Insert new data to db 
+                    postdb.created_at = Date.now();
+                    postdb.shortcode = postdata.shortcode;
+                    postdb.media = postdata;
+                    postdb.save(function (err) {
+                        if (err) return handleError(err);
+                        console.log(postdata.shortcode + " : saved");
+                    });
+                }
+            });
+        }
+        console.log(`\ntime: ${Date()}\nhashtag: ${config.hastags[tag]}\nstatus: finished\n`);
+    }
 
 }
 
 console.log('Start fetching ...');
 fetch();
+
 function intervalFunc() {
     fetch();
 }
 setInterval(intervalFunc, config.interval);
-
-
