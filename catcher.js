76b8fe4c398334957a40c2445d31c3a653cc4209
Korawit Prutsachainimmit <korawit.p@gmail.com>
2021-08-13 04:01:10 +0700

    Edit config for sandbox

--- catcher.js
+++ catcher.js
@@ -4,7 +4,7 @@ var Instagram = require('./lib/instafetch');
 var PostDocument = require('./lib/app/models/post');
 Instagram = new Instagram();
 
-var dburl = 'mongodb://mean.psu.ac.th:27017/ptie';
+var dburl = 'mongodb://localhost:27017/instagram';
 
 // Connect to DB
 mongoose.connect(dburl, {
@@ -14,30 +14,17 @@ mongoose.connect(dburl, {
 // Set custom promise to Bluebird
 mongoose.Promise = require('bluebird');
 
-// Connect to NETPIE
-var MicroGear = require('microgear');
-
-const APPID = 'PTEI';
-const KEY = 'zp4z7w9QoGFZgOd';
-const SECRET = 'CInR0O0133Pzxma5sFqHtRYRC';
-
-var microgear = MicroGear.create({
-    key: KEY,
-    secret: SECRET
-});
-
-microgear.on('connected', function() {
-    console.log('Connected to NETPIE');
-    microgear.setAlias("catcher");
+try {
     console.log('Start fetching ...');
     fetch();
     function intervalFunc() {
         fetch();
     }
     setInterval(intervalFunc, config.interval);
-});
-
-microgear.connect(APPID);
+}
+catch (error) {
+    console.error(error);
+}
 
 async function fetch() {
     //Fetch by Location
@@ -48,37 +35,35 @@ async function fetch() {
         console.log('Location[' + config.locations[loc] + '] entries: ' + entries.length);
         for (var i = 0; i < entries.length; i++) {
 
-            //console.log(entries[0]);
-            var postdata = await Instagram.getPostByShortcode(entries[i].node.shortcode);  //Instagram API updated found on 11 May 2018                       
-
-            //Store data to DB
-            var postdb = new PostDocument;
-            //Check if the selecte entry is existed in database
-            PostDocument.findOne({
-                shortcode: postdata.shortcode
-            }, function(err, exist) {
+            try {
+                
+                //Get each post from entries
+                const { node : postdata } = entries[i];                
+                
+                //Check if the selecte entry is existed in database
+                const exist = await PostDocument.findOne({shortcode: postdata.shortcode});
                 if (exist) {
                     console.log(postdata.shortcode + " : already existed");
                 } else {
-                    //Insert new data to db 
+                    //Store data to DB
+                    let postdb = new PostDocument;
                     postdb.created_at = Date.now();
                     postdb.shortcode = postdata.shortcode;
                     postdb.media = postdata;
-                    postdb.save(function(err) {
-                        if (err) return handleError(err);
-                        console.log(postdata.shortcode + " : saved");
-                    });
-                }
-            });
+                    await postdb.save();
+                    console.log(postdata.shortcode + " : saved");
+                }                
+            }
+            catch (error) {
+                console.error(error);
+            }
         }
     }
 
-    PostDocument.count({}, function(err, c) {
+    PostDocument.count({}, function (err, c) {
         if (err) console.log(err);
         console.log(`\ntime: ${Date()}\nlocation: ${config.locations[loc]}\nstatus: finished\n`);
         console.log(`Total : ${c}\n`);
-        microgear.publish("/instagram/post/total", `${c}`);
-        console.log("publish");
     });
 
     //Fetch by Hashtag
@@ -87,34 +72,35 @@ async function fetch() {
         var entries = hashtagdata.edges;
         console.log('Hashtag[' + config.hastags[tag] + '] entries: ' + entries.length);
         for (var i = 0; i < entries.length; i++) {
-            var postdata = await Instagram.getPostByShortcode(entries[i].node.shortcode);
-            //Store data to DB
-            var postdb = new PostDocument;
-            //Check if the selecte entry is existed in database
-            PostDocument.findOne({
-                shortcode: postdata.shortcode
-            }, function(err, exist) {
+
+            try {
+                
+                //Get each post from entries
+                const { node : postdata } = entries[i];                
+                
+                //Check if the selecte entry is existed in database
+                const exist = await PostDocument.findOne({shortcode: postdata.shortcode});
                 if (exist) {
                     console.log(postdata.shortcode + " : already existed");
                 } else {
-                    //Insert new data to db 
+                    //Store data to DB
+                    let postdb = new PostDocument;
                     postdb.created_at = Date.now();
                     postdb.shortcode = postdata.shortcode;
                     postdb.media = postdata;
-                    postdb.save(function(err) {
-                        if (err) return handleError(err);
-                        console.log(postdata.shortcode + " : saved");
-                    });
-                }
-            });
+                    await postdb.save();
+                    console.log(postdata.shortcode + " : saved");
+                }                
+            }
+            catch (error) {
+                console.error(error);
+            }
         }
     }
 
-    PostDocument.count({}, function(err, c) {
+    PostDocument.count({}, function (err, c) {
         if (err) console.log(err);
         console.log(`\ntime: ${Date()}\nhashtag: ${config.hastags[tag]}\nstatus: finished\n`);
         console.log(`Total : ${c}\n`);
-        microgear.publish("/instagram/post/total", `${c}`);
-        console.log("publish");
     });
 }
